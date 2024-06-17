7a7a8c1b88e0044a352757f6146fd1cef4c27321
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-18 00:40:07 +0700

    feat: signout link, edit link dropdown component

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+'use client'
+import React from 'react'
+import { SignOutButton } from '@clerk/nextjs'
+import { useToast } from '../ui/use-toast'
 
 function SignOutLink() {
-  return <div>SignOutLink</div>;
+  const { toast } = useToast()
+
+  const handleLogout = () => {
+    toast({
+      title: 'Signed out',
+      description: 'You have been signed out of your account.',
+    })
+  }
+
+  return (
+    <SignOutButton redirectUrl="/">
+      <button onClick={handleLogout} className="w-full text-left">
+        Logout
+      </button>
+    </SignOutButton>
+  )
 }
 
-export default SignOutLink;
+export default SignOutLink
