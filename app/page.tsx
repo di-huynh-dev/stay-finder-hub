1f57032c77b890bd16eae6bfc7ad52fe4bc4944c
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-17 21:55:02 +0700

    feat: add shadcn-ui component

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function HomePage() {
+  return (
+    <div>
+      HomePage
+      <Button variant={"outline"} size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default HomePage;
