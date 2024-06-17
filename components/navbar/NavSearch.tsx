ce2945f47ffda6c7df860cb828957d78e923fb48
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-17 23:24:16 +0700

    feat: add navbar

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,12 @@
+import { Input } from "../ui/input";
+
+function NavSearch() {
+  return (
+    <Input
+      type="search"
+      placeholder="Search destinations"
+      className="max-w-xs dark:bg-muted "
+    />
+  );
+}
+export default NavSearch;
