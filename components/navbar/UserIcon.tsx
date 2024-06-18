375dd065d0ea15e5fb0cd3e8318e5c86ad5f6cb5
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-18 16:44:56 +0700

    feat: fetch profile image action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,8 +1,14 @@
-import React from "react";
-import { LuUser2 } from "react-icons/lu";
+import React from 'react'
+import { LuUser2 } from 'react-icons/lu'
+import { fetchProfileImage } from '@/utils/actions'
+import Image from 'next/image'
 
-function UserIcon() {
-  return <LuUser2 className="h-6 w-6 bg-primary rounded-full text-white" />;
+async function UserIcon() {
+  const profileImage = await fetchProfileImage()
+  if (profileImage) {
+    return <img src={profileImage} className="h-6 w-6 object-cover rounded-full" alt="profile image" />
+  }
+  return <LuUser2 className="h-6 w-6 bg-primary rounded-full text-white" />
 }
 
-export default UserIcon;
+export default UserIcon
--- utils/actions.ts
+++ utils/actions.ts
@@ -39,3 +39,10 @@ export const createProfileAction = async (prevState: any, formDData: FormData) =
   }
   redirect('/')
 }
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser()
+  if (!user) return null
+  const profile = await db.profile.findUnique({ where: { clerkId: user.id }, select: { profileImage: true } })
+  return profile?.profileImage
+}
