d48df66e35abd55cac58cd6a529a4e3d8f092d34
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-21 14:03:12 +0700

    feat: handle nav search

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,12 +1,42 @@
-import { Input } from "../ui/input";
+'use client'
+import { Input } from '../ui/input'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
+import { useState, useEffect } from 'react'
 
 function NavSearch() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams)
+    if (value) {
+      params.set('search', value)
+    } else {
+      params.delete('search')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
+
+  useEffect(() => {
+    if (!searchParams.get('search')) {
+      setSearch('')
+    }
+  }, [searchParams.get('search')])
+
+  const [search, setSearch] = useState(searchParams.get('search')?.toString() || '')
   return (
     <Input
-      type="search"
+      type="text"
       placeholder="Search destinations"
-      className="max-w-xs dark:bg-muted "
+      className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value)
+        handleSearch(e.target.value)
+      }}
     />
-  );
+  )
 }
-export default NavSearch;
+export default NavSearch
