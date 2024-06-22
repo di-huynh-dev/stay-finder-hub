38adc93a12b50edd449d1f01835cf8745662c7ac
dihuynh@outlook.sg <dihuynh@outlook.sg>
2024-06-22 10:37:54 +0700

    feat: creat review model, set up actions, components

--- utils/actions.ts
+++ utils/actions.ts
@@ -235,6 +235,22 @@ export const fetchPropertyDetail = async ({ propertyId }: { propertyId: string }
     },
   })
 }
+
+export const createReviewAction = async () => {
+  return { message: 'create review' }
+}
+
+export const fetchPropertyReviews = async () => {
+  return { message: 'fetch reviews' }
+}
+
+export const fetchPropertyReviewsByUser = async () => {
+  return { message: 'fetch user reviews' }
+}
+
+export const deleteReviewAction = async () => {
+  return { message: 'delete  reviews' }
+}
 const renderError = (error: unknown): { message: string } => {
   return {
     message: error instanceof Error ? error.message : 'An error occurred',
